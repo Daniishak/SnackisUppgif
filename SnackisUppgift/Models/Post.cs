0ca3c2cf85c33caf3833628028f89ab15e781e37
Daniishak <danielishak10@hotmail.com>
2023-05-18 14:31:06 +0200

    latestfortoday(Fixa anonym)

--- SnackisUppgift/Models/Post.cs
+++ SnackisUppgift/Models/Post.cs
@@ -11,15 +11,15 @@ namespace SnackisUppgift.Models
 		public string? Image { get; set; }
 
 		[Required(ErrorMessage = "Title is required.")]
-		public string Title { get; set; }
+		public string? Title { get; set; }
 
 		[Required(ErrorMessage = "Description is required.")]
-		public string Description { get; set; }
+		public string? Description { get; set; }
 
 		public virtual Subject? Subject { get; set; }
 
 		[Required(ErrorMessage = "Subject is required.")]
-		public int SubjectId { get; set; }  // Make this non-nullable
+		public int? SubjectId { get; set; }  // Make this non-nullable
 
 		public DateTime? Date { get; set; }
 		public string? UserName { get; set; }
