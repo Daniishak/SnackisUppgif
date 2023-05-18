0dea384f4a65db6c54caa4b1d90856607eea6547
Daniishak <danielishak10@hotmail.com>
2023-05-18 13:34:06 +0200

    updatedMessagesDm

--- SnackisUppgift/Pages/DmIndex.cshtml.cs
+++ SnackisUppgift/Pages/DmIndex.cshtml.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using SnackisUppgift.Areas.Identity.Data;
@@ -29,6 +30,36 @@ namespace SnackisUppgift.Pages
             DirectMessages = await _context.DirectMessages
                 .Where(dm => dm.ReceiverId == userId || dm.SenderId == userId)
                 .ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostSendDMAsync(string receiverEmail, string dmContent)
+        {
+            // Find the receiver by their email
+            var receiver = await _userManager.FindByEmailAsync(receiverEmail);
+            if (receiver == null)
+            {
+                ModelState.AddModelError("", "No user with that email found");
+                return Page();
+            }
+
+            // Get the sender's ID
+            var senderId = _userManager.GetUserId(User);
+
+            // Create a new DM
+            var dm = new DirectMessage
+            {
+                SenderId = senderId,
+                ReceiverId = receiver.Id,
+                Message = dmContent,
+                SentAt = DateTime.UtcNow
+            };
+
+            _context.DirectMessages.Add(dm);
+            await _context.SaveChangesAsync();
+
+            // TODO: send a notification to the receiver
+
+            return RedirectToPage();
         }
     }
 }
